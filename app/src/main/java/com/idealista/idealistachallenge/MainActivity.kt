b4ec8c46b32b8f2b05816f4be7b542808519e557
Angel <angelgarciamoratalla98@gmail.com>
2024-10-16 12:32:12 +0200

    feat: Add MainActivity

---
+++ app/src/main/java/com/idealista/idealistachallenge/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.idealista.idealistachallenge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ComponentActivity
+
+class MainActivity : AppCompatActivity() {
+
+}
