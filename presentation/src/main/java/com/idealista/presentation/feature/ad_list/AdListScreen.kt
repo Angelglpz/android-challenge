cb421d5ad01c5aa774bf5017baa8d3c69477d8e3
Angel <angelgarciamoratalla98@gmail.com>
2024-10-17 01:01:44 +0200

    feat: Implement presentation module, add a basic compose class with correct colors. Add core module.

---
+++ presentation/src/main/java/com/idealista/presentation/feature/ad_list/AdListScreen.kt
@@ -0,0 +1,17 @@
+package com.idealista.presentation.feature.ad_list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AdListScreen() {
+    AdListTheme {
+
+    }
+}
+
+@Preview
+@Composable
+fun AdListScreenPreview() {
+    AdListScreen()
+}
---
+++ presentation/src/main/java/com/idealista/presentation/feature/ad_list/ComposeTheme.kt
@@ -0,0 +1,35 @@
+package com.idealista.presentation.feature.ad_list
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import com.idealista.core.R
+
+@Composable
+fun customLightColorScheme() = lightColorScheme(
+    primary = colorResource(id = R.color.green_light),
+    onPrimary = colorResource(id = R.color.green_dark),
+    secondary = colorResource(id = R.color.magenta_light),
+    onSecondary = colorResource(id = R.color.magenta_dark),
+    background = colorResource(id = R.color.white)
+)
+
+@Composable
+fun customDarkColorScheme() = darkColorScheme(
+    primary = colorResource(id = R.color.green_dark),
+    onPrimary = colorResource(id = R.color.green_light),
+    secondary = colorResource(id = R.color.magenta_dark),
+    onSecondary = colorResource(id = R.color.magenta_light),
+    background = colorResource(id = R.color.black)
+)
+
+@Composable
+fun AdListTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = if (darkTheme) customDarkColorScheme() else customLightColorScheme(),
+        content = content
+    )
+}
